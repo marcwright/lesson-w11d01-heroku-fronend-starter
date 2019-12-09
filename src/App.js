f8cbb5eb84a32429e24d2d090a2d5ff8fc9f57e2
Marc Wright <marc.wright@gmail.com>
2019-12-09 13:07:00 -0500

    console log process.env

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
 
   render() {
     console.log(this.state.users)
-    console.log("Rendered")
+    console.log(process.env)
     return (
       <div className="App" >
         <header className="App-header">
