8586d81c6121bdbde0f7f3c69164b3831f1cca74
Marc Wright <marc.wright@gmail.com>
2019-12-09 12:49:21 -0500

    change http to https

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 // const databaseUrl = process.env.HEROKU_DB_URL || 'http://localhost:3000'
-const databaseUrl = 'http://heroku-deployment-backend2.herokuapp.com'
+const databaseUrl = 'https://heroku-deployment-backend2.herokuapp.com'
 
 class App extends React.Component {
   state = {
